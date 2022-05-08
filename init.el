a554d59c6409a48620e83cbfce95d7bc1f17d8f6
Eduardo Miyashiro Asato <ed.miya.a@gmail.com>
2022-05-08 18:29:29 +0200

    Initial commit

---
+++ init.el
@@ -0,0 +1,34 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(use-package doom-themes
+  :ensure t
+  :config
+  (load-theme 'doom-one t))
+
+(use-package evil
+  :ensure t
+  :config
+  (evil-mode))
+
+(use-package ivy
+  :ensure t
+  :config
+  (setq ivy-re-builders-alist
+        '((t . ivy--regex-fuzzy)))
+  (ivy-mode))
+
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(setq inhibit-startup-screen t)
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+(global-display-line-numbers-mode)
+(global-whitespace-mode)
+
+(global-set-key (kbd "<f6>") (lambda() (interactive)(find-file user-init-file)))
